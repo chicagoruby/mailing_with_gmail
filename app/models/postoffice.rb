1f674a4f70aeda1379cb8eb71dfca5067caa7ae9
Matt Polito <matt.polito@gmail.com>
2008-10-16 21:19:20 -0500

    documented some methods

--- app/models/postoffice.rb
+++ app/models/postoffice.rb
@@ -1,14 +1,16 @@
 class Postoffice < ActionMailer::Base
   def welcome(name, email)
-    @recipients = "matt.polito@gmail.com"
-    @from       = "chicagoruby@reactioncontrol.com"
+    @recipients   = "matt.polito@gmail.com"
+    @from         = "chicagoruby@reactioncontrol.com"
     headers         "Reply-to" => "#{email}"
-    @subject    = "ChicagoRuby.org from Gmail"
-    @sent_on    = Time.now
+    @subject      = "ChicagoRuby.org from Gmail"
+    @sent_on      = Time.now
     @content_type = "text/html"
     
-    body[:name] = name
-    body[:email] = email
+    body[:name] t  = name
+    body[:email]  = email
   end
+  
+  # PostOffice.deliver_welcome("Matt Polito", "matt.polito@gmail.com")
 
 end
